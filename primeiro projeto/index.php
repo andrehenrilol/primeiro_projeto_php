9be1bc6622f36147ef70c2a1f487a7423f30b8a5
andrehenrilol <99298130+andrehenrilol@users.noreply.github.com>
2023-02-27 16:45:28 -0300

    Add files via upload

---
+++ primeiro projeto/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+
+        <?php
+        echo "ola mundo";
+        ?>
+    </h1>
+    <h2>
+        4+3 = 
+        <?php
+        echo (4+2)
+        ?>
+    </h2>
+        
+    <h2>
+    4-3 =
+        <?php
+        echo (4-3)
+        ?>
+    </h2>
+
+    <h2>
+    4*3 =
+        <?php
+        echo (4*3)
+        ?>
+    </h2>
+    <h2>
+    9/3 =
+        <?php
+        echo (9/3)
+        ?>
+    </h2>
+</body>
+</html>
